aa24092a9d684a17630738c4873d9ee951865425
Alan Sangma <asangma@esri.com>
2015-10-21 20:05:48 -0700

    init

---
+++ alansangma.com/index-bak-2015-10-21.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- 
+////////////////////////////////////////////////
+SITE DESIGN & DEVELOPMENT: Alan Sangma
+(909) 844-2321
+alansangma@gmail.com
+////////////////////////////////////////////////
+-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <title>Alan Sangma : Designer / Developer / Director</title>
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link rel="icon" href="/favicon.png" type="image/png" sizes="64x64"/>
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom -->
+    <link href="css/screen.css" rel="stylesheet">
+  </head>
+  <body id="body-el" data-560="background-color: rgb(0,0,0);" data-570="background-color: rgb(240,240,255);" data-650="background-color: rgb(33,33,33);"  data-end="background-color: rgb(0,0,0);">
+  <div id="skrollr-body">
+
+
+          <div class="container-fluid">
+            <div class="row text-center" id="title-row" data-top="background-position-y:0%;" data--300-top="background-position-y:100%;">
+              <div class="col-md-6 col-md-offset-3">
+                <h1 class="page-title">Alan Sangma</h1>
+                <p class="page-subtitle">DESIGNER</p>
+                <img src="images/Alan-Sangma-900x900-bw.jpg" alt="Portrait of Alan Sangma" id="portrait" data-0="top:0px;" data-300="top:-50px;" />
+              </div>
+            </div>
+
+            <div class="row text-center" style="margin-bottom:100px;">
+              <div class="col-md-8 col-md-offset-2">
+                <p class="lead" data-0="transform:scale(1);opacity:1;" data-500="transform:scale(1.25);opacity:0;">I am all about the F word.</p>  
+                <p  ><i class="fa fa-arrow-down fa-2x" id="arrow-down" data-0="top:20px;transform:scale(1);opacity:1;"  data-400="top:300px;opacity:0;transform[cubic]:scale(0.5);"></i></p>
+              </div>
+            </div>
+            <div class="row text-center">
+              <div class="col-md-12" id="focus-illustration">
+                <img src="images/focus/focus-lens.png" alt="lens image" id="focus-lens" data-100="opacity:0;top:-50px;"  data-400="opacity:1;top:0px;"/>
+                <img src="images/focus/focus-ray-top-0.png" alt="light ray image" class="focus-ray" id="focus-ray-0" data-350="opacity:0;top:20px;transform: scale(0.5,1);" data-550="opacity:1;top:0px;transform[cubic]: scale(1,1);"/>
+                <img src="images/focus/focus-ray-top-1.png" alt="light ray image" class="focus-ray" id="focus-ray-1" data-500="opacity:0;top:20px;transform: scale(0.5,1);" data-550="opacity:1;top:0px;transform[cubic]: scale(1,1);"/>
+                <img src="images/focus/focus-ray-bottom-0.png" alt="light ray image" class="focus-ray" id="focus-ray-3" data-450="opacity:0;top:-20px;transform: scale(0.5,1);" data-550="opacity:1;top:0px;transform: scale(1,1);"/>
+                <img src="images/focus/focus-ray-bottom-1.png" alt="light ray image" class="focus-ray" id="focus-ray-4" data-400="opacity:0;top:-10px;transform: scale(0.5,1);" data-550="opacity:1;top:0px;transform: scale(1,1);"/>
+                <img src="images/focus/focus-flare-main.png" alt="lens flare image" id="focus-flare-0" data-540="opacity:0;transform[bounce]: scale(0);" data-580="opacity:1;transform[bounce]: scale(1);"/>
+                <img src="images/focus/focus-flare-bg.png" alt="lens flare image" id="focus-flare-1" data-350="opacity:0;transform: scale(0);" data-550="opacity:1;transform: scale(1);"/>
+                <img src="images/focus/focus-lens-cast.png" alt="lens flare image" id="focus-lens-cast" data-400="opacity:0;" data-550="opacity:1;"/>
+                <img src="images/focus/poly-bg-00-30-opacity.png" class="focus-poly-bg" alt="polygon pattern background" data-300="opacity:0;top:-75px;" data-550="opacity:1;top:0px;" data-1175="top:190px;"/>
+              </div>
+            </div>
+
+            <div class="row text-center">
+              <div class="col-md-12">
+                <h1 id="focus-title">FOCUS</h1>
+              </div>
+              <div class="col-md-6 col-md-offset-3">
+                <p class="lead" data-750="opacity:0;" data-820="opacity:1;">Focus is power. When we focus on the user, we make good things that empower the user to do great things!</p>
+              </div>
+
+            </div>
+            <div class="row" data-300-end="opacity:1;background-position-y:-200%;" data-100-end="opacity:1;background-position-y[cubic]:0%;" id="bottom-sky">
+              <div class="col-md-5 col-md-offset-1" style="position:relative;" data-800="top:50px;" data--100-end="top:0px;">
+                <h3>DEVELOPMENT</h3>
+                <hr>
+                <p>Every perfect solution has an expiration date.  Users change.  The digital ecosystem changes.  The real ecosystem changes.  Holding too tightly to any particular solution shifts focus away from the reason for the solution: the user.</p>
+                <p>I am always asking myself, “How can we help this person?”  Sometimes, the answer is removing a feature.  Other times it is writing code that a remote developer can easily read.  Whatever the case, the benefit of the user is always the focus.</p>
+                <ul class="mobile-skills skills-development"  style="margin-top:2em;">
+                  <li></li>
+                  <li><i class="fa fa-cog"></i> HTML5/CSS3</li>
+                  <li><i class="fa fa-cog"></i> Javascript</li>
+                  <li><i class="fa fa-cog"></i> PHP/MySQL</li>
+                  <!-- <li><i class="fa fa-cog"></i> APIs</li> -->
+                </ul>
+              </div>
+              <div class="col-md-5" style="position:relative;" data-800="top:50px;" data--100-end="top:0px;">
+                <h3>DESIGN</h3>
+                <hr>
+                <p>It is easy to get lost when making decisions about the flow of a user interface or the layout of a print asset.  Ideas can come from anywhere and knowing which ideas to develop for a particular user group requires focus.</p>
+                <p>That&rsquo;s why I always focus on the goals, mode, and environment of the end user or viewer.  It simplifies the decision making process and lets us put more of our creative energies into making remarkable designs.</p>
+                <ul class="mobile-skills skills-design" style="margin-top:2em;">
+                  <li><i class="fa fa-star"></i> UI &amp; UX</li>
+                  <li><i class="fa fa-star"></i> Creative Direction</li>
+                  <li><i class="fa fa-star"></i> Brand Identity</li>
+                  <!-- <li><i class="fa fa-star"></i> Graphic Design</li> -->
+                </ul>
+              </div>
+
+              <div class="col-md-6 col-md-offset-3" style="text-align:center;margin-top:50px;" data-140-end="opacity:0;" data-25-end="opacity:1;">
+                <div class="col-md-4">
+                  <a href="https://www.behance.net/alan357c" target="_blank" type="button" class="btn btn-default btn-lg cta-btn" title="View Portfolio on Behance">
+                    <i class="fa fa-briefcase"></i> PORTFOLIO
+                  </a>
+                </div>
+                <div class="col-md-4 col-md-offset-2">
+                  <a href="docs/Alan-Sangma-Resume-2015-02-18.00.pdf" target="_blank" type="button" class="btn btn-default btn-lg cta-btn" title="view Resume PDF">
+                    <i class="fa fa-file-pdf-o"></i> RESUM&Eacute;
+                  </a>
+                </div>
+              </div>
+              <div id="contact-card">
+                <h1>Alan Sangma</h1>
+                <ul class="skills skills-development">
+                  <li></li>
+                  <li><i class="fa fa-cog"></i> HTML5/CSS3</li>
+                  <li><i class="fa fa-cog"></i> Javascript</li>
+                  <li><i class="fa fa-cog"></i> PHP/MySQL</li>
+                </ul>
+
+                
+                  <a class="telephone" href="tel:9098442321"><i class="fa fa-phone"></i> (909) 844-2321</a>
+                  <a class="email" href="mailto:alansangma@gmail.com"><i class="fa fa-envelope"></i> alansangma@gmail.com</a>
+                  <a class="linkedin" href="https://www.linkedin.com/in/alansangma"><i class="fa fa-linkedin-square"></i> LinkedIn</a>
+                
+
+                <ul class="skills skills-design">
+                  <li><i class="fa fa-star"></i> UI &amp; UX</li>
+                  <li><i class="fa fa-star"></i> Design</li>
+                  <li><i class="fa fa-star"></i> Branding</li>
+                </ul>
+              </div>
+          </div>
+        </div>
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery-1.11.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/skrollr.min.js" type="text/javascript" charset="utf-8"></script>
+    <!-- SCRIPTS -->
+
+    <script type="text/javascript" charset="utf-8">
+    jQuery(document).ready(function(e) {
+
+      ////////////////////////////////////////////////
+      //  SETUP THE FOCUS TEXT ANIMATION
+      var time = 600;
+      var str = jQuery('#focus-title').html();
+      jQuery('#focus-title').empty();
+      jQuery.each(str.split(''), function(index, val) {
+         var input = '<span data-'+time+'="opacity:0;filter:blur(10px);" data-'+(time+50)+'="opacity:1;filter:blur(0px);">'+val+'</span>';
+         
+         jQuery('#focus-title').append(input);
+         if(val !== 'F') time += 10;
+         else time += 150;
+         
+      });
+      ////////////////////////////////////////////////
+
+      ////////////////////////////////////////////////
+      //  INIT SKROLLR
+      var sklr = skrollr.init({
+        smoothScrollling    :false,
+        forceHeight         :false
+      });
+      ////////////////////////////////////////////////
+    });
+    </script>
+  </body>
+</html>
---
+++ alansangma.com/js/portfolio.js
@@ -0,0 +1,59 @@
+jQuery(document).ready(function($) {
+
+    ////////////////////////////////////////////////
+    //  INIT SKROLLR
+    var sklr;
+    /* = skrollr.init({
+            smoothScrollling: true,
+            forceHeight: true
+        });*/
+    ////////////////////////////////////////////////
+
+
+
+    var loc = window.location.href;
+    var start = loc.lastIndexOf('/') + 1;
+    var end = loc.length;
+    var page = loc.substring(start, end);
+    var isHome = (page.length == 0 || page == 'index.html') ? true : false;
+    //console.log(page + ' : ' + isHome);
+    //console.log(isHome);
+    //jQuery('')
+
+    jQuery.get('includes/nav.html', function(data, textStatus, xhr) {
+        var window_w = jQuery(window).width();
+        jQuery('.body').prepend(data);
+        if (!isHome) {
+            jQuery('.nav li').each(function(index, elem) {
+                var href = jQuery(elem).children('a').attr('href');
+                if (href == page) {
+                    jQuery(elem).addClass('active');
+                    if (jQuery(elem).attr('data-parent')) {
+                        jQuery('.' + jQuery(elem).attr('data-parent')).addClass('active');
+                    }
+                }
+            });
+        } else {
+            jQuery('#about').addClass('active');
+            //data-top="margin-left:-65px;" data--150-top="margin-left:-15px;"
+            jQuery('.navbar-brand').attr('data--200-top', "margin-left:-75px;");
+            jQuery('.navbar-brand').attr('data--250-top', "margin-left:-25px;");
+        }
+       jQuery('body').animate({scrollTop: 0}, 25, function(){
+          //  console.log('*');
+            sklr = skrollr.init({
+                smoothScrollling: true,
+                forceHeight: true,
+                edgeStrategy: 'ease'
+            }); 
+       });
+    });
+
+
+    jQuery(window).resize(function(e) {
+        var w = jQuery('.project-banner').width();
+        jQuery('.project-banner').height(w / 2.5);
+    });
+    jQuery(window).trigger('resize');
+
+});
